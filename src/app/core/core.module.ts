906dfb555695c59bd465cf92113e1d90543102ac
baby7305 <songhaibei7305@163.com>
2018-10-12 23:09:50 +0800

    配置根模块

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -7,4 +7,10 @@ import { CommonModule } from '@angular/common';
   ],
   declarations: []
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已经存在，不能再次加载！')
+    }
+  }
+}
