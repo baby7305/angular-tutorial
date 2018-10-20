15af50f67f396e7e5d9a7895213818b700f00824
baby7305 <songhaibei7305@163.com>
2018-10-20 14:42:35 +0800

    改变头顶按钮样式，字体图标、透明

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   exports: [HeaderComponent, FooterComponent, SidebarComponent]
