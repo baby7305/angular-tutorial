541bb98bf5f7455afffc72698bd99ae83ac2f971
baby7305 <songhaibei7305@163.com>
2018-11-19 19:13:54 +0800

    route配置official模块换掉默认主页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+
+const appRoutes: Routes = [
+  { path: '', component: OfficialComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import { OfficialComponent } from './official/official.component';
     FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
-    EasyUIModule
+    EasyUIModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
